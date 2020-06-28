c43eb9367fbfe2b09fa3b72614be869ab9aa6230
tom <mark@qq.com>
2020-06-28 23:55:08 +0800

    我的第一次提交

---
+++ AjaxController.java
@@ -0,0 +1,10 @@
+package com.itheima.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("product-list")
+public class AjaxController {
+
+}
